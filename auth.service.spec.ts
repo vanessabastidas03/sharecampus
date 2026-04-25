54f1bf3c9d82937923e37a9d9aed8c2057d22cca
Vanessa Bastidas <134403943+vanessabastidas03@users.noreply.github.com>
2026-04-25 06:39:16 -0500

    Updated auth.service.spec.ts on 2026-04-25

---
+++ auth.service.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  const mockUsersService = {
+    findByEmail: jest.fn(),
+    findByVerificationToken: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+
+  const mockJwtService = {
+    sign: jest.fn(),
+    verify: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: mockUsersService },
+        { provide: JwtService, useValue: mockJwtService },
+      ],
+    }).compile();
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
